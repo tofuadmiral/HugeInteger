f0c8a9a60f6ac65146fb8bf68bd23cb88496bd80
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-06 11:40:58 -0400

    added runtime analysis files as well as graphs, added report

---
+++ src/MergeSort.java
@@ -0,0 +1,17 @@
+public class MergeSort {
+
+    // implementing mergesort in java
+    // sort right half, sort left half, then sort
+
+    public int[] msort(int[] array, int l, int m, int r){
+        // sort array[] using mergesort
+        // what we want to do is take the left and right sides, and merge recursively
+        if(array.length==1){
+            // this is our base case
+            return array;
+        }
+        else{
+
+        }
+    }
+}
